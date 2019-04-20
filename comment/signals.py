dd1533d1ff818f590e31945f927401628579f30a
edwin <475308964@qq.com>
2019-04-20 13:32:38 +0800

    部署配置

--- comment/signals.py
+++ comment/signals.py
@@ -5,7 +5,8 @@ from notifications.signals import notify
 from django.utils.html import strip_tags
 from django.template.loader import render_to_string
 from django.core.mail import send_mail
-from BlogProject import settings
+# from BlogProject import settings
+from django.conf import settings
 from .models import Comment
 
 @receiver(post_save, sender=Comment)
