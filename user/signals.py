7de06e656d7b32fbe93c3ee5e2b37b586cdff3c3
edwin <475308964@qq.com>
2019-04-21 18:52:52 +0800

    解决新用户注册成功,站内通知消息点击出错的bug

--- user/signals.py
+++ user/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.urls import reverse
 from notifications.signals import notify
 from django.contrib.auth.models import User
 
@@ -8,5 +9,6 @@ def send_notification(sender, instance, **kwargs):
     # 注册成功,发送站内消息通知
     if kwargs['created'] == True:
         verb = '恭喜您注册成功,更多精彩内容等你发现'
-        notify.send(instance, recipient=instance, verb=verb)
+        url = reverse('user_info')
+        notify.send(instance, recipient=instance, verb=verb, url=url)
 
